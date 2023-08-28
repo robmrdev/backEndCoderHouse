50032cf54fa25ccefbdbbc0aa5a16fa5c8e5fd9d
Robert Mendoza <robmrivas@gmail.com>
2023-08-28 16:04:15 -0300

    correccion segundo desafio

--- desafioDos/fileSystem.js
+++ desafioDos/fileSystem.js
@@ -10,46 +10,81 @@ class ProductManager {
     static id = 0
 
     addProduct = async (title, description, price, thumbmail, code, stock) => {
-
-        ProductManager.id++
-
-        let newProduct = { title, description, price, thumbmail, code, stock, id: ProductManager.id }
-
-        this.products.push(newProduct)
-
-        await fs.writeFile(this.path, JSON.stringify(this.products))
+        try {
+            ProductManager.id++
+            let newProduct = { title, description, price, thumbmail, code, stock, id: ProductManager.id }
+            this.products = await this.getProducts()
+            this.products.push(newProduct)
+            await fs.writeFile(this.path, JSON.stringify(this.products))
+        }
+        catch (err) {
+            console.log(`addProduct fails`)
+            return true
+        }
     }
 
+    
     readProducts = async () => {
         let respuesta = await (fs.readFile(this.path, "utf-8"))
         return JSON.parse(respuesta)
     }
 
-    getProducts = async () => {
-        let respGetProduct = await this.readProducts();
-        return console.log(respGetProduct)
-    }
 
-    getProductsById = async (id) => {
-        let respId = await this.readProducts();
-        if (!respId.find(product => product.id === id)) {
-            console.log("Not Found")
-        } else {
-            console.log(respId.find(product => product.id === id))
+    getProducts = async () => {
+        try {
+            let products = await fs.readFile(this.path, "utf-8")
+            return JSON.parse(products)
+        }
+        catch (err) {
+            console.log(`getProducts fails`)
+            return []
         }
     }
 
-    updateProduct = async ({ id, ...updatedProduct }) => {
-        await this.deleteProduct(id);
-        let productsBefore = await this.readProducts()
-        let modidifyProduct = [{ ...updatedProduct, id }, ...productsBefore]
-        await fs.writeFile(this.path, JSON.stringify(modidifyProduct))
+    getProductsById = async (id) => {
+        try {
+            let respId = await this.readProducts();
+            if (!respId.find(product => product.id === id)) {
+                console.log("Not Found")
+            } else {
+                console.log(respId.find(product => product.id === id))
+            }
+        }
+        catch (err) {
+            console.log(`getProductsById fails`)
+            return false
+        }
+    }
+
+    updateProduct = async ({ id, title, description, price, thumbmail, code, stock }) => {
+        try {
+            if (!title || !description || !price || !thumbmail || !code || !stock) {
+                console.log('Todos los campos son requeridos para actualizar un producto.')
+                return;
+            }
+
+            await this.deleteProduct(id);
+            let productsBefore = await this.readProducts()
+            let modifiedProduct = [{ id, title, description, price, thumbmail, code, stock }, ...productsBefore];
+            await fs.writeFile(this.path, JSON.stringify(modifiedProduct))
+        }
+        catch (err) {
+            console.log(`updateProduct fails`)
+            return false
+        }
     }
 
     deleteProduct = async (id) => {
-        let respDelete = await this.readProducts();
-        let filterDelete = respDelete.filter(products => products.id != id)
-        await fs.writeFile(this.path, JSON.stringify(filterDelete))
+        try {
+            let respDelete = await this.readProducts();
+            let filterDelete = respDelete.filter(products => products.id != id)
+            await fs.writeFile(this.path, JSON.stringify(filterDelete))
+            return true
+        }
+        catch (err) {
+            console.log(`deleteProduct fails`)
+            return false
+        }
     }
 }
 
@@ -61,24 +96,26 @@ const product = new ProductManager;
 
 // PASO 1, AGREGAR PRODUCTOS
 
-// product.addProduct('Producto 1','Descripcion de producto 1',1500,'https://www.productos.com/producto1.png','a1a1a1a1a1',30)
-// product.addProduct('Producto 2','Descripcion de producto 2',3000,'https://www.productos.com/producto2.png','b2b2b2b2b2',30)
-// product.addProduct('Producto 3','Descripcion de producto 3',4000,'https://www.productos.com/producto3.png','c3c3c3c3c3',30)
+// await product.addProduct('Producto 1','Descripcion de producto 1',1500,'https://www.productos.com/producto1.png','a1a1a1a1a1',30)
+// await product.addProduct('Producto 2','Descripcion de producto 2',3000,'https://www.productos.com/producto2.png','b2b2b2b2b2',30)
+// await product.addProduct('Producto 3','Descripcion de producto 3',4000,'https://www.productos.com/producto3.png','c3c3c3c3c3',30)
 
 
 
 
 //PASO 2, Obtener los productos
 
-// product.getProducts()
+// let products = await product.getProducts()
+// console.log(products)
 
 
 
 
 // PASO 3, Borrar productos
 
-// product.deleteProduct(2)
-// product.getProducts()
+// let products = await product.getProducts()
+// await product.deleteProduct(2)
+// console.log(products)
 
 
 
@@ -86,15 +123,16 @@ const product = new ProductManager;
 
 //PASO 4, ACTUALIZAR DATOS DE UN PRODUCTO
 
-// product.updateProduct({
-//     title: 'Producto 1 MOFIDICADO',
+// await product.updateProduct({
+//     title: 'Producto 1 MOFIDICADO2',
 //     description: 'Descripcion de producto 1 MODIFICADO',
-//     price: 1500,
+//     price: 343434
 //     thumbmail: 'https://www.productos.com/producto1.png',
 //     code: 'a1a1a1a1a1',
 //     stock: 30,
 //     id: 1
 // })
 
-// product.getProducts()
+// let products = await product.getProducts()
+// console.log(products)
 
